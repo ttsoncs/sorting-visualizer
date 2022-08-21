f8067109b2c77b5328d11de5d8a1b0dbc808099d
mariejie <tson25102002@gmail.com>
2022-08-22 02:52:27 +0700

    New commit

--- src/SortAlgorithms/shellSort.h
+++ src/SortAlgorithms/shellSort.h
@@ -19,24 +19,21 @@ class ShellSort : public SortStrategy {
     ShellSort &operator=(ShellSort &&other) noexcept = default;
 
     void sort() override {
-        std::vector<std::pair<float, Color>> v_ = vector_.getVector();
-        uint32_t gap{static_cast<uint32_t>(v_.size())};
-        while (gap > 1) {
-            gap = static_cast<uint32_t>(static_cast<double>(gap) / 1.3);
-            for (uint32_t i{gap}; i < v_.size(); ++i) {
-                auto [value, color] = v_[i];
-                uint32_t j{i};
-                while (j >= gap && v_[j - gap].first > value) {
-                    v_[j] = v_[j - gap];
+        auto v = vector_.getVector();
+        auto size{v.size()};
+        for (auto gap{size / 2}; gap > 0; gap /= 2) {
+            for (auto i{gap}; i < size; ++i) {
+                auto j{i};
+                while (j >= gap && v[j].first < v[j - gap].first) {
+                    Visualize::visualizeBar(v, j, "Shell Sort");
+                    Visualize::visualizeBar(v, j - gap, "Shell Sort");
+                    std::swap(v[j], v[j - gap]);
                     j -= gap;
+                    Visualize::visualizeVector(v, "Shell Sort");
                 }
-                Visualize::visualizeBar(v_, i, "Shell Sort");
-                v_[j] = std::make_pair(value, color);
-                Visualize::visualizeBar(v_, j, "Shell Sort");
-                Visualize::visualizeVector(v_, "Shell Sort");
             }
         }
-        Visualize::visualizeEnding(v_, "Shell Sort");
+        Visualize::visualizeEnding(v, "Shell Sort");
     }
 };
 
